3651e6ad4fcda37b433c5daa480e8c67dffdf91b
mirfan6 <124807832+mirfan6@users.noreply.github.com>
2023-02-08 21:56:42 +0500

    Add files via upload

---
+++ cpp25.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=1;
+	switch (a){
+		case 1:
+			cout<<"a";
+			break;
+			case 2:
+				cout<<"b";
+				break;
+				case 3:
+				cout<<"c";
+				break;
+				
+	}
+	return 0;
+}
---
+++ cpp28.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int sum1=54+78;
+	int sum2=sum1+95;
+	int sum3=sum1+sum2;
+	cout<<sum1 << "\n";
+	cout<<sum2 << "\n";
+	cout<<sum3;
+	return 0;
+}
